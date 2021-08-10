0e85b6799fb0ebdc4faa50ec7b04b87f6d0fbdad
Not-SiriusA <filip.grzymski@gmail.com>
2021-08-10 23:18:50 +0200

    commit

--- src/notepad/urls.py
+++ src/notepad/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_view, name='dashboard')
+    path('', views.dashboard_view, name='dashboard'),
+    path('view/<uuid:uuid>', views.note_entire_view, name='note_entire'),
 ]
--- src/notepad/views.py
+++ src/notepad/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import redirect, render
-
 from django.contrib.auth.decorators import login_required
+
+from .models import Note, NoteShare, Profile, User
 
 @login_required(login_url='/login/')
 def dashboard_view(request, *args, **kwargs):
-    context = {
 
+    user_profile = Profile.objects.filter(user=request.user)[0]
+
+    notes = Note.objects.filter(owner=user_profile)
+
+    context = {
+        'user_profile': user_profile,
+        'notes': notes,
     }
 
     return render(request, 'notepad/dashboard.html', context)
+
+
+@login_required(login_url='/login/')
+def note_entire_view(request, *args, **kwargs):
+    # uuid should be passed in the url like "..path/<uuid>/"
+    UUID = kwargs['uuid']
+
+    note = Note.objects.filter(id=UUID)[0]
+
+    context = {
+        'note': note
+    }
+
+    return render(request, 'notepad/note_entire.html', context)
