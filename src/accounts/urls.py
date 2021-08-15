ea0140fb01faf582093a8fd6f2e211e80dd5f6b1
NotSirius-A <filip.grzymski@gmail.com>
2021-08-15 08:15:18 -0700

    styling homepage

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_page_view, name='login_page'),
-    path('signup/', views.user_creation_view, name='user_creation'),
+    path('signup/', views.user_creation_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
 ]
